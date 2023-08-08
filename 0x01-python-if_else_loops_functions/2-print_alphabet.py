429af3a8df794026a4992cf41ca99930da9ca44a
PrinceMag <princeth.magutshwa@gmail.com>
2023-08-08 14:54:47 +0200

    update
    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-number = 97
-while number < 123:
+for number in range(97, 123):
     print("{:s}".format(chr(number)), end='')
-    number += 1
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-digit1 = 0
-digit2 = 1
-while digit1 <= 8:
-    while digit2 <= 9:
-        if digit1 != digit2:
-            print("{:d}".format(digit1), end='')
-            if digit1 != 8:
-                print("{:d}, ".format(digit2), end='')
-            else:
-                print("{:d}".format(digit2))
-        digit2 += 1
-    digit2 = digit1 + 1
-    digit1 = digit1 + 1
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(f"{i:02}, {j:02}", end='')
+        break
+    if i < 8:
+        print(', ', end='')
+    else:
+        print()
