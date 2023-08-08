99c9424ab65240bd5759fee3e38a5d5def2f9ac1
PrinceMag <princeth.magutshwa@gmail.com>
2023-08-08 14:44:32 +0200

    Update
    Update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+number = 97
+while(number < 123):
+    if 113 == number or 101 == number:
+        number += 1
+        continue
+    print("{:s}".format(chr(number)), end='')
+    number += 1
